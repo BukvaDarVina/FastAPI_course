8855664df977005a7483a126503b24daaa6d85e3
Danil Burdukovskii <bdv99@mail.ru>
2024-11-18 13:32:46 +0900

    fifth commit (post hotel in repo)

--- repositories/hotels.py
+++ repositories/hotels.py
@@ -1,4 +1,5 @@
-from sqlalchemy import select, func
+from pydantic import BaseModel
+from sqlalchemy import select, func, insert
 
 from repositories.base import BaseRepository
 from src.database import engine
@@ -25,7 +26,12 @@ class HotelsRepository(BaseRepository):
             .limit(limit)
             .offset(offset)
         )
-        print(query.compile(engine, compile_kwargs={"literal_binds": True}))
+        # print(query.compile(engine, compile_kwargs={"literal_binds": True}))
         result = await self.session.execute(query)
 
         return result.scalars().all()
+
+    async def add(self, hotel_data: HotelsORM):
+        add_hotel_stmt = insert(self.model).values(**hotel_data.model_dump())
+        await self.session.execute(add_hotel_stmt)
+        return hotel_data
