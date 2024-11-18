a3aa6a66715e341efe31cb4b03a064c3f63bebe2
Danil Burdukovskii <bdv99@mail.ru>
2024-11-18 16:59:27 +0900

    eighth commit (datamapper)

--- repositories/hotels.py
+++ repositories/hotels.py
@@ -1,9 +1,8 @@
-from pydantic import BaseModel
-from sqlalchemy import select, func, insert
+from sqlalchemy import select, func
 
 from repositories.base import BaseRepository
-from src.database import engine
 from src.models.hotels import HotelsORM
+from src.schemas.hotels import Hotel
 
 
 class HotelsRepository(BaseRepository):
@@ -29,4 +28,4 @@ class HotelsRepository(BaseRepository):
         # print(query.compile(engine, compile_kwargs={"literal_binds": True}))
         result = await self.session.execute(query)
 
-        return result.scalars().all()
+        return [Hotel.model_validate(hotel, from_attributes=True) for hotel in result.scalars().all()]
--- src/api/hotels.py
+++ src/api/hotels.py
@@ -3,7 +3,7 @@ from fastapi import Query, APIRouter, Body
 from repositories.hotels import HotelsRepository
 from src.api.dependencies import PaginationDep
 from src.database import async_session_maker
-from src.schemas.hotels import Hotel, HotelPATCH
+from src.schemas.hotels import HotelPATCH, HotelAdd
 
 router = APIRouter(prefix="/hotels", tags=["Отели"])
 
@@ -31,7 +31,7 @@ async def get_hotel(hotel_id: int):
 
 
 @router.post("", summary="Добавление отеля")
-async def create_hotel(hotel_data: Hotel = Body(openapi_examples={
+async def create_hotel(hotel_data: HotelAdd = Body(openapi_examples={
     "1": {"summary": "Сочи", "value": {
         "title": "Отель Rich 5 звезд у моря",
         "location": "г. Сочи, ул. Моря, д. 1",
@@ -50,7 +50,7 @@ async def create_hotel(hotel_data: Hotel = Body(openapi_examples={
 
 
 @router.put("/{hotel_id}", summary="Полное обновление данных об отеле")
-async def edit_hotel(hotel_id: int, hotel_data: Hotel):
+async def edit_hotel(hotel_id: int, hotel_data: HotelAdd):
     async with async_session_maker() as session:
         await HotelsRepository(session).edit(hotel_data, id=hotel_id)
         await session.commit()
