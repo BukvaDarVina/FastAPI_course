89916b3342c23972229e9144e59cce24ab20c9a8
Danil Burdukovskii <bdv99@mail.ru>
2024-11-19 11:36:43 +0900

    ninth commit (auth)

---
+++ src/migrations/versions/2024_11_19_1133-c35792ba1aca_add_colums_users_table.py
@@ -0,0 +1,27 @@
+"""add colums users table
+
+Revision ID: c35792ba1aca
+Revises: 731546103ae2
+Create Date: 2024-11-19 11:33:36.402013
+
+"""
+
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = "c35792ba1aca"
+down_revision: Union[str, None] = "731546103ae2"
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    op.create_unique_constraint(None, "users", ["email"])
+
+
+def downgrade() -> None:
+    op.drop_constraint(None, "users", type_="unique")
