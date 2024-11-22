8c009da95dbc16b6d49a948f3f0b4993cfbe3cc0
Danil Burdukovskii <bdv99@mail.ru>
2024-11-22 11:41:13 +0900

    fifteenth commit (add facilities)

---
+++ src/api/facilities.py
@@ -0,0 +1,30 @@
+from fastapi import APIRouter, Body
+
+from src.api.dependencies import DBDep, PaginationDep
+from src.schemas.facilities import FacilitiesAdd, Facilities
+
+router = APIRouter(prefix="/facilities", tags=["Удобства"])
+
+
+@router.get("", summary="Справочник удобств")
+async def get_all_facilities(db: DBDep, pagination: PaginationDep):
+    per_page = pagination.per_page or 5
+    return await db.facilities.get_all(
+        limit=per_page,
+        offset=per_page * (pagination.page - 1)
+    )
+
+
+@router.post("", summary="Добавление в справочник удобств")
+async def create_facilities(db: DBDep, facilities_data: FacilitiesAdd = Body(openapi_examples={
+    "1": {"summary": "Wi-fi", "value": {
+        "title": "Wi-fi"
+    }},
+    "2": {"summary": "Бар", "value": {
+        "title": "Бар"
+    }},
+})
+                            ):
+    facilities = await db.facilities.add(facilities_data)
+    await db.commit()
+    return {"status": "OK", "data": facilities}
---
+++ src/migrations/versions/2024_11_22_1106-6172f0825444_add_facilities.py
@@ -0,0 +1,48 @@
+"""add facilities
+
+Revision ID: 6172f0825444
+Revises: 5daf305fcd13
+Create Date: 2024-11-22 11:06:15.830658
+
+"""
+
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = "6172f0825444"
+down_revision: Union[str, None] = "5daf305fcd13"
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    op.add_column(
+        "room_facilities", sa.Column("facilities_id", sa.Integer(), nullable=False)
+    )
+    op.drop_constraint(
+        "room_facilities_facilities_fkey", "room_facilities", type_="foreignkey"
+    )
+    op.create_foreign_key(
+        None, "room_facilities", "facilities", ["facilities_id"], ["id"]
+    )
+    op.drop_column("room_facilities", "facilities")
+
+
+def downgrade() -> None:
+    op.add_column(
+        "room_facilities",
+        sa.Column("facilities", sa.INTEGER(), autoincrement=False, nullable=False),
+    )
+    op.drop_constraint(None, "room_facilities", type_="foreignkey")
+    op.create_foreign_key(
+        "room_facilities_facilities_fkey",
+        "room_facilities",
+        "facilities",
+        ["facilities"],
+        ["id"],
+    )
+    op.drop_column("room_facilities", "facilities_id")
